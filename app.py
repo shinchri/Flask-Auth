436cf75a72d4d60aebd0340ba1658bbaa57d88d3
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-07-28 17:36:11 -0500

    feat: Create basic sqlalchemy application

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template
+from flask_security.datastore import SQLAlchemyUserDatastore
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemySessionUserDatastore, UserMixin, RoleMixin, login_required
+
+# Create app
+app = Flask(__name__)
+app.config['DEBUG']=True
+app.config['SECRET_KEY']='super-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+
+# create database connection object
+db = SQLAlchemy(app)
+
+# Define Models
+roles_users = db.Table('roles_users', 
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+)
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime)
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+
+# Setup Flask Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    db.create_all()
+    user_datastore.create_user(email='matt@nobien.net', password='password')
+    db.session.commit()
+
+# Views
+@app.route('/')
+def home():
+    user_list = User.query.all()
+
+    datas = []
+    for user in user_list:
+        data={
+            "id": user.id,
+            "email": user.email,
+            "password": user.password
+        }
+        datas.append(data)
+
+    return render_template('index.html', users=datas)
+
+if __name__ = '__main__':
+    app.run()
